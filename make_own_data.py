6feb4b4a7a8b202c15ed5d9c982557ef09d99ad6
ZhiHao Qian <lacusrinz@gmail.com>
2018-05-24 21:30:45 +0800

    start train

--- make_own_data.py
+++ make_own_data.py
@@ -14,8 +14,15 @@ classes = {'-1', '0', '1'}  # 人为设定3类
 stocks_list = {'601878'}
 writer = tf.python_io.TFRecordWriter("up_or_down_train.tfrecords")  # 要生成的文件
 
-for index, symbol in enumerate(stocks_list):
-    class_path = cwd + symbol + '/'
+for symbol in enumerate(stocks_list):
+
+    writer = tf.python_io.TFRecordWriter("up_or_down_train.tfrecords")  # 要生成的文件
+    class_path = cwd + str(symbol) + '/'
+
+    df = pd.read_csv(class_path + str(symbol) + '_final.csv', sep=',')
+    labels = df['day_five']
+    labels_list = labels.tolist()
+
     for i, img_name in os.listdir(class_path):
         img_path = class_path + img_name + '.png'# 每一个图片的地址
         print(img_path)
@@ -28,15 +35,30 @@ for index, symbol in enumerate(stocks_list):
         data_encode = np.array(img_encode)
         img_raw = data_encode.tostring()
 
-        df = pd.read_csv(class_path + symbol + '_final.csv', sep=',')
-        labels = df['day_five']
-        labels_list = labels.tolist()
+
         print(labels_list[i])
 
         # example = tf.train.Example(features=tf.train.Features(feature={
-        #     "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
+        #     "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[labels_list[i]])),
         #     'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
         # }))  # example对象对label和image数据进行封装
         # writer.write(example.SerializeToString())  # 序列化为字符串
+        writer.close()
 
-writer.close()
+
+def read_and_decode(filename):  # up_or_down_train.tfrecords
+    filename_queue = tf.train.string_input_producer([filename])  # 生成一个queue队列
+
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)  # 返回文件名和文件
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                           'img_raw': tf.FixedLenFeature([], tf.string),
+                                       })  # 将image数据和label取出来
+
+    img_decode = tf.decode_raw(features['img_raw'], tf.uint8)
+    img_decode = tf.reshape(img_decode, [128, 128, 3])  # reshape为128*128的3通道图片
+    img_decode = tf.cast(img_decode, tf.float32) * (1. / 255) - 0.5  # 在流中抛出img张量
+    label = tf.cast(features['label'], tf.int32)  # 在流中抛出label张量
+    return img_decode, label
---
+++ up_down_train.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import numpy as np
+import make_own_data
+
+batch_size = 50
