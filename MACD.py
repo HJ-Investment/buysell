0c3219208bdb032f1b3dbbf71663f28713bfaa72
ZhiHao Qian <lacusrinz@gmail.com>
2018-05-30 23:24:34 +0800

    update macd model

--- MACD.py
+++ MACD.py
@@ -1,63 +1,285 @@
-# import matplotlib
-#
-# import tushare as ts
-#
-# import pandas as pd
-#
-# import matplotlib.pyplot as plt
-#
-# fig=plt.gcf()
-#
-# df=ts.get_hist_data('000001',start='2015-11-01',end='2015-12-31')
-#
-# with pd.plotting.plot_params.use('x_compat',True):
-#     df.high.plot(color='r',figsize=(10,4),grid='on')
-#     df.low.plot(color='b',figsize=(10,4),grid='on')
-#     fig.savefig('E:\\Python')
+# -*- coding: utf-8 -*-
 
-#######以上是画分时图
-
-
-import matplotlib.pyplot as plt
+import tensorflow as tf
 import numpy as np
-import talib
-import tushare as ts
-from numpy import nan as NaN
-import pandas as pd
+import make_own_data
+import re
+import os
 
-fig=plt.gcf()
-df=ts.get_k_data('600000',start='2015-01-01', end='2018-05-03',)
-close = [float(x) for x in df['close']]
-# 调用talib计算指数移动平均线的值
-df['EMA12'] = talib.EMA(np.array(close), timeperiod=12)
-df['EMA26'] = talib.EMA(np.array(close), timeperiod=26)
- # 调用talib计算MACD指标,返回的MACD是DIF，快线；MACDsignal是DEA值，慢线；macd=dif-DEA画成柱状图
-df['MACD'],df['MACDsignal'],df['MACDhist'] = talib.MACD(np.array(close),
-                            fastperiod=12, slowperiod=26, signalperiod=9)
+FLAGS = tf.app.flags.FLAGS
 
-df=df.fillna(method='bfill')
-# df.to_csv('E://stock_data.csv', index=True,sep='\t', encoding='utf-8')
+# Basic model parameters.
+tf.app.flags.DEFINE_integer('batch_size', 128,
+                            """Number of images to process in a batch.""")
+tf.app.flags.DEFINE_string('data_dir', '/tmp/xxx',
+                           """Path to the MACD data directory.""")
+tf.app.flags.DEFINE_boolean('use_fp16', False,
+                            """Train the model using fp16.""")
 
-##talib返回的MACD是DIF，快线；MACDsignal是DEA值，慢线；macd=dif-DEA画成柱状图
-macd_df = df[["date","MACD","MACDsignal","MACDhist"]]
-# print(macd_df.head(50))
+# IMAGE_SIZE = make_own_data.IMAGE_SIZE
+# NUM_CLASSES = make_own_data.NUM_CLASSES
+# NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = make_own_data.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+# NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = make_own_data.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 
 
-print(macd_df.head(50))
-# macd_df.date=pd.to_datetime(macd_df.date)
-# macd_df_date = macd_df.set_index("date")
-macd_df_date_60 = macd_df.tail(60)
-# print(macd_df_date_60)
+# Constants describing the training process.
+MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
+NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
+LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
+INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
+
+# If a model is trained with multiple GPUs, prefix all Op names with tower_name
+# to differentiate the operations. Note that this prefix is removed from the
+# names of the summaries when visualizing a model.
+TOWER_NAME = 'tower'
 
 
+def _activation_summary(x):
+  """Helper to create summaries for activations.
 
-# with pd.plotting.plot_params.use('x_compat',True):
-#     plt.xticks(rotation=20)
-#     plt.grid=True
-#     # macd_df_date_60.MACD.plot(color='r',figsize=(10,4),grid='on')#快线
-#     plt.plot(macd_df_date_60.index, macd_df_date_60['MACD'], label='macd dif')#快线
-#     plt.plot(macd_df_date_60.index, macd_df_date_60['MACDsignal'], label='signal dea')# 慢线
-#     plt.bar(macd_df_date_60.index, macd_df_date_60['MACDhist']*2, label='hist bar')
-#     fig.savefig('E:\\MACD001')
+  Creates a summary that provides a histogram of activations.
+  Creates a summary that measures the sparsity of activations.
+
+  Args:
+    x: Tensor
+  Returns:
+    nothing
+  """
+  # Remove 'tower_[0-9]/' from the name in case this is a multi-GPU training
+  # session. This helps the clarity of presentation on tensorboard.
+  tensor_name = re.sub('%s_[0-9]*/' % TOWER_NAME, '', x.op.name)
+  tf.summary.histogram(tensor_name + '/activations', x)
+  tf.summary.scalar(tensor_name + '/sparsity',
+                                       tf.nn.zero_fraction(x))
 
 
+def _variable_on_cpu(name, shape, initializer):
+  """Helper to create a Variable stored on CPU memory.
+
+  Args:
+    name: name of the variable
+    shape: list of ints
+    initializer: initializer for Variable
+
+  Returns:
+    Variable Tensor
+  """
+  with tf.device('/cpu:0'):
+    dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+    var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+  return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+  """Helper to create an initialized Variable with weight decay.
+
+  Note that the Variable is initialized with a truncated normal distribution.
+  A weight decay is added only if one is specified.
+
+  Args:
+    name: name of the variable
+    shape: list of ints
+    stddev: standard deviation of a truncated Gaussian
+    wd: add L2Loss weight decay multiplied by this float. If None, weight
+        decay is not added for this Variable.
+
+  Returns:
+    Variable Tensor
+  """
+  dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+  var = _variable_on_cpu(
+      name,
+      shape,
+      tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
+  if wd is not None:
+    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+    tf.add_to_collection('losses', weight_decay)
+  return var
+
+
+def inputs(eval_data):
+  """Construct input for MACD evaluation using the Reader ops.
+
+  Args:
+    eval_data: bool, indicating if one should use the train or eval data set.
+
+  Returns:
+    images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
+    labels: Labels. 1D tensor of [batch_size] size.
+
+  Raises:
+    ValueError: If no data_dir
+  """
+  if not FLAGS.data_dir:
+    raise ValueError('Please supply a data_dir')
+  data_dir = os.path.join(FLAGS.data_dir, 'xxx')
+  images, labels = make_own_data.inputs(eval_data=eval_data,
+                                        data_dir=data_dir,
+                                        batch_size=FLAGS.batch_size)
+  if FLAGS.use_fp16:
+    images = tf.cast(images, tf.float16)
+    labels = tf.cast(labels, tf.float16)
+  return images, labels
+
+def inference(images):
+  """Build the MACD model.
+
+  Args:
+    images: Images returned from distorted_inputs() or inputs().
+
+  Returns:
+    Logits.
+  """
+  # We instantiate all variables using tf.get_variable() instead of
+  # tf.Variable() in order to share variables across multiple GPU training runs.
+  # If we only ran this model on a single GPU, we could simplify this function
+  # by replacing all instances of tf.get_variable() with tf.Variable().
+  #
+  # conv1
+  with tf.variable_scope('conv1') as scope:
+    kernel = _variable_with_weight_decay('weights',
+                                         shape=[5, 5, 3, 64],
+                                         stddev=5e-2,
+                                         wd=0.0)
+    conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
+    biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
+    pre_activation = tf.nn.bias_add(conv, biases)
+    conv1 = tf.nn.relu(pre_activation, name=scope.name)
+    _activation_summary(conv1)
+
+  # pool1
+  pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                         padding='SAME', name='pool1')
+  # norm1
+  norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                    name='norm1')
+
+  # conv2
+  with tf.variable_scope('conv2') as scope:
+    kernel = _variable_with_weight_decay('weights',
+                                         shape=[5, 5, 64, 64],
+                                         stddev=5e-2,
+                                         wd=0.0)
+    conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
+    biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+    pre_activation = tf.nn.bias_add(conv, biases)
+    conv2 = tf.nn.relu(pre_activation, name=scope.name)
+    _activation_summary(conv2)
+
+  # norm2
+  norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                    name='norm2')
+  # pool2
+  pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
+                         strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+
+  # local3
+  with tf.variable_scope('local3') as scope:
+    # Move everything into depth so we can perform a single matrix multiply.
+    reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
+    dim = reshape.get_shape()[1].value
+    weights = _variable_with_weight_decay('weights', shape=[dim, 384],
+                                          stddev=0.04, wd=0.004)
+    biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+    local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+    _activation_summary(local3)
+
+  # local4
+  with tf.variable_scope('local4') as scope:
+    weights = _variable_with_weight_decay('weights', shape=[384, 192],
+                                          stddev=0.04, wd=0.004)
+    biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+    local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+    _activation_summary(local4)
+
+  # linear layer(WX + b),
+  # We don't apply softmax here because
+  # tf.nn.sparse_softmax_cross_entropy_with_logits accepts the unscaled logits
+  # and performs the softmax internally for efficiency.
+  with tf.variable_scope('softmax_linear') as scope:
+    weights = _variable_with_weight_decay('weights', [192, NUM_CLASSES],
+                                          stddev=1/192.0, wd=0.0)
+    biases = _variable_on_cpu('biases', [NUM_CLASSES],
+                              tf.constant_initializer(0.0))
+    softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
+    _activation_summary(softmax_linear)
+
+  return softmax_linear
+
+def loss(logits, labels):
+  """Add L2Loss to all the trainable variables.
+
+  Add summary for "Loss" and "Loss/avg".
+  Args:
+    logits: Logits from inference().
+    labels: Labels from distorted_inputs or inputs(). 1-D tensor
+            of shape [batch_size]
+
+  Returns:
+    Loss tensor of type float.
+  """
+  # Calculate the average cross entropy loss across the batch.
+  labels = tf.cast(labels, tf.int64)
+  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=labels, logits=logits, name='cross_entropy_per_example')
+  cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+  tf.add_to_collection('losses', cross_entropy_mean)
+
+  # The total loss is defined as the cross entropy loss plus all of the weight
+  # decay terms (L2 loss).
+  return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+  def train(total_loss, global_step):
+  """Train MACD model.
+
+  Create an optimizer and apply to all trainable variables. Add moving
+  average for all trainable variables.
+
+  Args:
+    total_loss: Total loss from loss().
+    global_step: Integer Variable counting the number of training steps
+      processed.
+  Returns:
+    train_op: op for training.
+  """
+  # Variables that affect learning rate.
+  num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
+  decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+
+  # Decay the learning rate exponentially based on the number of steps.
+  lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+                                  global_step,
+                                  decay_steps,
+                                  LEARNING_RATE_DECAY_FACTOR,
+                                  staircase=True)
+  tf.summary.scalar('learning_rate', lr)
+
+  # Generate moving averages of all losses and associated summaries.
+  loss_averages_op = _add_loss_summaries(total_loss)
+
+  # Compute gradients.
+  with tf.control_dependencies([loss_averages_op]):
+    opt = tf.train.GradientDescentOptimizer(lr)
+    grads = opt.compute_gradients(total_loss)
+
+  # Apply gradients.
+  apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+  # Add histograms for trainable variables.
+  for var in tf.trainable_variables():
+    tf.summary.histogram(var.op.name, var)
+
+  # Add histograms for gradients.
+  for grad, var in grads:
+    if grad is not None:
+      tf.summary.histogram(var.op.name + '/gradients', grad)
+
+  # Track the moving averages of all trainable variables.
+  variable_averages = tf.train.ExponentialMovingAverage(
+      MOVING_AVERAGE_DECAY, global_step)
+  variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+  with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
+    train_op = tf.no_op(name='train')
+
+  return train_op
--- make_own_data.py
+++ make_own_data.py
@@ -9,6 +9,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.misc
 
+# image size of 128 x 128. If one alters this number, then the entire model
+# architecture will change and any model would need to be retrained.
+IMAGE_SIZE = 128
+
+# Global constants describing the MACD/KDJ data set.
+NUM_CLASSES = 3
+NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
+NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = 10000
+
+
 
 classes = {'-1', '0', '1'}  # 人为设定3类
 stocks_list = {'601328','600999', '601628', '600016', '601985', '600019', '600028', '601668', '601878', '601688', '601669', '601390', '601211', '601006', '601989', '601988', '600104', '600518', '601398', '600958', '600309', '601857','600030', '601318', '600837', '600036', '600050', '600547', '601766', '601166', '601601', '601229', '600919', '601169', '600029', '601800', '600000', '600887', '600340', '601881', '601088','603993', '600519', '601336', '601186', '600606', '601818', '600048', '600111', '601288' }
@@ -16,10 +26,7 @@ stocks_list = {'601328','600999', '601628', '600016', '601985', '600019', '60002
 # writer = tf.python_io.TFRecordWriter("up_or_down_train.tfrecords")  # 要生成的文件
 
 def encode_and_write(stocks_list):
-
     cwd = 'F:\Code\\buysell\data\pic_data\\'
-    
-
     for _, symbol in enumerate(stocks_list):
         writer = tf.python_io.TFRecordWriter("F:\Code\\buysell\data\pic_data\\macd_train_" + symbol + ".tfrecords")  # 要生成的文件
 
@@ -75,30 +82,105 @@ def read_and_decode(filename):  # up_or_down_train.tfrecords
     label = tf.cast(features['label'], tf.int32)  # 在流中抛出label张量
     return img_decode, label
 
+
+def _generate_image_and_label_batch(image, label, min_queue_examples,
+                                    batch_size):
+  """Construct a queued batch of images and labels.
+
+  Args:
+    image: 3-D Tensor of [height, width, 3] of type.float32.
+    label: 1-D Tensor of type.int32
+    min_queue_examples: int32, minimum number of samples to retain
+      in the queue that provides of batches of examples.
+    batch_size: Number of images per batch.
+
+  Returns:
+    images: Images. 4D tensor of [batch_size, height, width, 3] size.
+    labels: Labels. 1D tensor of [batch_size] size.
+  """
+  num_preprocess_threads = 16
+  images, label_batch = tf.train.batch(
+        [image, label],
+        batch_size=batch_size,
+        num_threads=num_preprocess_threads,
+        capacity=min_queue_examples + 3 * batch_size)
+
+  # Display the training images in the visualizer.
+  tf.summary.image('images', images)
+
+  return images, tf.reshape(label_batch, [batch_size])
+
+def inputs(eval_data, data_dir, batch_size):
+  """Construct input for MACD/KDJ evaluation using the Reader ops.
+
+  Args:
+    eval_data: bool, indicating if one should use the train or eval data set.
+    data_dir: Path to the MACD/KDJ data directory.
+    batch_size: Number of images per batch.
+
+  Returns:
+    images: Images. 4D tensor of [batch_size, IMAGE_SIZE, IMAGE_SIZE, 3] size.
+    labels: Labels. 1D tensor of [batch_size] size.
+  """
+  if not eval_data:
+    filenames = [os.path.join(data_dir, 'data_batch_%d.bin' % i)]
+    num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
+  else:
+    filenames = [os.path.join(data_dir, 'test_batch.bin')]
+    num_examples_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+  for f in filenames:
+    if not tf.gfile.Exists(f):
+      raise ValueError('Failed to find file: ' + f)
+
+  # Create a queue that produces the filenames to read.
+  filename_queue = tf.train.string_input_producer(filenames)
+
+  # Read examples from files in the filename queue.
+  image, label = read_and_decode('F:\Code\\buysell\data\pic_data\\macd_train_601878.tfrecords')
+
+  # Subtract off the mean and divide by the variance of the pixels.
+  float_image = tf.image.per_image_standardization(image)
+
+  # Set the shapes of tensors.
+  float_image.set_shape([height, width, 3])
+  label.set_shape([1])
+
+  # Ensure that the random shuffling has good mixing properties.
+  min_fraction_of_examples_in_queue = 0.4
+  min_queue_examples = int(num_examples_per_epoch *
+                           min_fraction_of_examples_in_queue)
+
+  # Generate a batch of images and labels by building up a queue of examples.
+  return _generate_image_and_label_batch(float_image, .label,
+                                         min_queue_examples, batch_size)
+
+
+
+
 # encode_and_write(stocks_list)
 
+# with tf.Session() as sess:
+#     reshaped_image = read_and_decode('F:\Code\\buysell\data\pic_data\\macd_train_601878.tfrecords')
+#     # 这一步start_queue_runner很重要。
+#     # 我们之前有filename_queue = tf.train.string_input_producer(filenames)
+#     # 这个queue必须通过start_queue_runners才能启动
+#     # 缺少start_queue_runners程序将不能执行
+#     threads = tf.train.start_queue_runners(sess=sess)
+#     # 变量初始化
+#     sess.run(tf.global_variables_initializer())
 
-with tf.Session() as sess:
-    reshaped_image = read_and_decode('F:\Code\\buysell\data\pic_data\\macd_train_601878.tfrecords')
-    # 这一步start_queue_runner很重要。
-    # 我们之前有filename_queue = tf.train.string_input_producer(filenames)
-    # 这个queue必须通过start_queue_runners才能启动
-    # 缺少start_queue_runners程序将不能执行
-    threads = tf.train.start_queue_runners(sess=sess)
-    # 变量初始化
-    sess.run(tf.global_variables_initializer())
-
-    if not os.path.exists('F:\Code\\buysell\data\pic_data\\raw_test'):
-        os.makedirs('F:\Code\\buysell\data\pic_data\\raw_test')
-    # 保存30张图片
-    for i in range(5):
-        # 每次sess.run(reshaped_image)，都会取出一张图片
-        img, label = sess.run(reshaped_image)
-        print(img.dtype)
-        print(sess.run(tf.shape(img)))
-        # 将图片保存
-        # ds = pd.DataFrame(img.tolist())
-        # ds.to_csv('F:\Code\\buysell\data\pic_data\\raw_test\\%d.csv' % i)
-        # print(img)   
-        # cv2.imwrite('F:\Code\\buysell\data\pic_data\\raw_test\\%d.png' % i, img)
-        print(label)
+#     if not os.path.exists('F:\Code\\buysell\data\pic_data\\raw_test'):
+#         os.makedirs('F:\Code\\buysell\data\pic_data\\raw_test')
+#     # 保存30张图片
+#     for i in range(5):
+#         # 每次sess.run(reshaped_image)，都会取出一张图片
+#         img, label = sess.run(reshaped_image)
+#         print(img.dtype)
+#         print(sess.run(tf.shape(img)))
+#         # 将图片保存
+#         # ds = pd.DataFrame(img.tolist())
+#         # ds.to_csv('F:\Code\\buysell\data\pic_data\\raw_test\\%d.csv' % i)
+#         # print(img)   
+#         # cv2.imwrite('F:\Code\\buysell\data\pic_data\\raw_test\\%d.png' % i, img)
+#         print(label)
