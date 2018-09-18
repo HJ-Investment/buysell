d92a1cd5ed8d30563b7649e351822de474a033b5
ZhiHao Qian <lacusrinz@gmail.com>
2018-09-18 23:45:16 +0800

    multiprocessing draw

--- multiprocessing_draw.py
+++ multiprocessing_draw.py
@@ -1,4 +1,4 @@
-from multiprocessing import Pool
+import multiprocessing
 from jaqs.data import DataApi
 import pandas as pd
 
@@ -9,10 +9,10 @@ def create_macd_j_pic(symbol, df, sequence):
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
 
-    if not os.path.exists('./data/prepared/pic_data/macd_j_pic/up'):
-        os.makedirs('./data/prepared/pic_data/macd_j_pic/up')
-    if not os.path.exists('./data/prepared/pic_data/macd_j_pic/notup'):
-        os.makedirs('./data/prepared/pic_data/macd_j_pic/notup')
+    if not os.path.exists('./data/prepared/pic_data/macd_j_pic2/up'):
+        os.makedirs('./data/prepared/pic_data/macd_j_pic2/up')
+    if not os.path.exists('./data/prepared/pic_data/macd_j_pic2/notup'):
+        os.makedirs('./data/prepared/pic_data/macd_j_pic2/notup')
 
     fig = plt.figure(figsize=(12.8, 12.8))
     df.reset_index(drop=True, inplace=True)
@@ -29,36 +29,40 @@ def create_macd_j_pic(symbol, df, sequence):
 
     str_symbol = symbol[:-3]
     if df['close_five'][2] >= 0.05:
-        fig.savefig('./data/prepared/pic_data/macd_j_pic/up/' + str_symbol + '_' + str(sequence), dpi=20)
+        fig.savefig('./data/prepared/pic_data/macd_j_pic2/up/' + str_symbol + '_' + str(sequence), dpi=20)
     else:
-        fig.savefig('./data/prepared/pic_data/macd_j_pic/notup/' + str_symbol + '_' + str(sequence), dpi=20)
+        fig.savefig('./data/prepared/pic_data/macd_j_pic2/notup/' + str_symbol + '_' + str(sequence), dpi=20)
 
     plt.close()
 
     print(sequence)
 
-api = DataApi(addr="tcp://data.quantos.org:8910")
-result, msg = api.login("18652420434", "eyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVfdGltZSI6IjE1MTcwNjAxMDgyOTMiLCJpc3MiOiJhdXRoMCIsImlkIjoiMTg2NTI0MjA0MzQifQ.b1ejSpbEVS7LhbsveZ5kvbWgUs7fnUd0-CBakPwNUu4")
-# print(result)
-data, msg = api.query(
-        view="lb.indexCons",
-        fields="symbol",
-        filter="index_code=000016.SH&start_date=20180801&end_date=20180831",
-        data_format='pandas')
-print(msg)
-symbols = data['symbol'].tolist()
-print(symbols)
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    pool = multiprocessing.Pool()
+    
+    api = DataApi(addr="tcp://data.quantos.org:8910")
+    result, msg = api.login("18652420434", "eyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVfdGltZSI6IjE1MTcwNjAxMDgyOTMiLCJpc3MiOiJhdXRoMCIsImlkIjoiMTg2NTI0MjA0MzQifQ.b1ejSpbEVS7LhbsveZ5kvbWgUs7fnUd0-CBakPwNUu4")
+    # print(result)
+    data, msg = api.query(
+            view="lb.indexCons",
+            fields="symbol",
+            filter="index_code=000016.SH&start_date=20180801&end_date=20180831",
+            data_format='pandas')
+    print(msg)
+    symbols = data['symbol'].tolist()
+    print(symbols)
 
-pool = Pool()
-for sym in symbols:
-    df = pd.read_csv('./data/prepared/datacsv/' + sym + '.csv', sep=',')
-    df.index = pd.to_datetime(df['trade_date'], format='%Y/%m/%d')
-    df.reset_index(drop=True, inplace=True)
-    j = df['j']
-    df['norm_j'] = j.apply(lambda x: (x - j.mean()) / (j.std()))
-    bar_value = df['MACDhist'] * 2
-    df['norm_bar'] = bar_value.apply(lambda x: (x - bar_value.mean()) / (bar_value.std()))
 
-    for i in range(len(df) - 8):
-        ts = df[0 + i:3 + i]
-        pool.apply(create_macd_j_pic, args=(sym, ts, i))
+    for sym in symbols:
+        df = pd.read_csv('./data/prepared/datacsv/' + sym + '.csv', sep=',')
+        df.index = pd.to_datetime(df['trade_date'], format='%Y/%m/%d')
+        df.reset_index(drop=True, inplace=True)
+        j = df['j']
+        df['norm_j'] = j.apply(lambda x: (x - j.mean()) / (j.std()))
+        bar_value = df['MACDhist'] * 2
+        df['norm_bar'] = bar_value.apply(lambda x: (x - bar_value.mean()) / (bar_value.std()))
+
+        for i in range(len(df) - 8):
+            ts = df[0 + i:3 + i]
+            pool.apply(create_macd_j_pic, args=(sym, ts, i))
