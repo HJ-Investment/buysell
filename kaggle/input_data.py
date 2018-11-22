187a0bc96274a481842f70a2d7b9ff7b17a2dc31
ZhiHao Qian <lacusrinz@gmail.com>
2018-11-22 21:22:21 +0800

    test

--- kaggle/input_data.py
+++ kaggle/input_data.py
@@ -27,19 +27,35 @@ def read(df):
     iterator = dataset.make_one_shot_iterator()
     image, label = iterator.get_next()
 
-    # image_raw = tf.reshape(image, [28, 28])
+    image_raw = tf.reshape(list(image.values()), [28, 28])
 
-    return image, label
+    images, label_batch = tf.train.batch(
+        [image_raw, label],
+        batch_size=32,
+        num_threads=64,
+        capacity=2000)
+
+    n_classes = 10
+    label_batch = tf.one_hot(label_batch, depth=n_classes)
+    label_batch = tf.cast(label_batch, dtype=tf.int32)
+    label_batch = tf.reshape(label_batch, [32, n_classes])
+
+    return images, label_batch
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     reshaped_image = read(df)
 
-    for i in range(5):
+    for i in range(1):
         # 每次sess.run(reshaped_image)，都会取出一张图片
-        img, label = sess.run(reshaped_image)
-        print(img)
-        # np.savetxt("prediction" + str(i) + ".csv", img.toarray(), delimiter=",")
-        print(label)
+        imgs, labels = sess.run(reshaped_image)
+        k = 0
+        for img in imgs:
+            np.savetxt("prediction" + str(k) + ".csv", img, delimiter=",")
+            k += 1
+        for label in labels:
+            print(label)
+        # np.savetxt("prediction" + str(i) + ".csv", img, delimiter=",")
+        # print(label)
 
 # np.savetxt("prediction.csv", image_raw, delimiter=",")
